de61767a7f4cf190fab1b6363ab368ad35ee776a
JairRamosG <ramosgonzalezjair@gmail.com>
2026-03-01 11:32:47 -0600

    creacion de la pagina en home en forma de una funcion

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,9 +1,18 @@
 from taipy.gui import Gui, State
-import taipy.gui.builder as tbg
+import taipy.gui.builder as tgb
 import plotly.express as px
 import pandas as pd
 import taipy as tp 
 
-with tbg.Page() as home_page:
-    tbg.text("Dashboard COvid 19 México", mode = "md")
-    tbg.text("En construcción...", mode = "md")
+def build_page():
+    '''
+    COnstrucción de toda la parte visual de la página
+    '''
+    with tgb.Page() as page:
+        tgb.text("Dashboard Covid 19 México", mode = "md")
+        tgb.html("hr")
+
+    return page
+
+##### Exportar página
+home_page = build_page()
